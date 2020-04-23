b364a19f9cc43077701ee26accd6a8080c97af8f
Joao Neto <jhonneto1221@gmail.com>
2020-04-23 02:19:51 -0300

    JWT update

--- app.py
+++ app.py
@@ -1,42 +1,51 @@
 import os, json
-from flask import Flask, jsonify, request, abort, redirect, url_for
+from flask import Flask, jsonify, request#, abort, redirect, url_for
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 app = Flask(__name__)
 
+# Setup the Flask-JWT-Extended extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
+jwt = JWTManager(app)
+
+# test route
 @app.route('/', methods = ["GET","POST"])
 def index():
 	print(request)
 	return request.data
-	
-@app.route('/login', methods = ["POST"])
+
+# Provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token, and you can return
+# it to the caller however you choose.
+@app.route('/login', methods=['POST'])
 def login():
-	print(request.data)
-	data = json.loads(request.data)
-	if data["user"] == "admin" and data["pwd"] == "admin":
-		auth = {
-			"auth": True,
-			"token": "token",
-			"message": "Logged in succesfully."
-		}
-		return jsonify(auth)
-	auth = {
-		"auth": False,
-		"token": None,
-		"message": "Invalid credentials."
-	}
-	return jsonify(auth)
+    if not request.is_json:
+        return jsonify({ "auth": False, "token": None, "message": "Missing JSON in request" }), 400
+
+    username = request.json.get('user', None)
+    password = request.json.get('pwd', None)
+    if not username:
+        return jsonify({ "auth": False, "token": None, "message": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({ "auth": False, "token": None, "message": "Missing password parameter"}), 400
+
+    if username != 'admin' or password != 'admin':
+        return jsonify({ "auth": False, "token": None, "message": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username)
+    return jsonify({ "auth": True, "token": access_token, "message": "Logged in succesfully."}), 200
 
 
-def verifyToken(token):
-	return token == "token"
-
-def checkLogin(request):
-	if not verifyToken(request.headers.get('access-token')):
-		abort(401)
-	
-@app.route('/teste', methods = ["GET"])
+# Protect a view with jwt_required, which requires a valid access token
+# in the request to access.
+@app.route('/teste', methods=['GET'])
+@jwt_required
 def teste():
-	checkLogin(request)
 	um = int(request.args.get('um', '0') or '0', 10)
 	dois = int(request.args.get('dois', '0') or '0', 10)
 	tres = int(request.args.get('tres', '0') or '0', 10)
@@ -48,8 +57,12 @@ def teste():
 					tres=tres,
 					quatro=quatro,
 					cinco=cinco,
-					seis=seis)
-		
-if __name__ == "__main__":
+					seis=seis), 200
+    # Access the identity of the current user with get_jwt_identity
+    #current_user = get_jwt_identity()
+    #return jsonify(logged_in_as=current_user), 200
+
+
+if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
